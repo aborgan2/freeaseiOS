af20d90f69078017dd27cb3698e195630f98e98c
Andrew Borgan <andrewborgan88@gmail.com>
2020-07-18 08:48:01 -0400

    added markers

--- components/FreebMarker.js
+++ components/FreebMarker.js
@@ -1,7 +1,26 @@
 import * as React from 'react';
+import { StyleSheet, Image } from 'react-native';
 import { Marker } from 'react-native-maps';
+import Coffee from '../Assets/Coffee_Marker.png';
+import Candy from '../Assets/Candy_Marker.png';
+import Food from '../Assets/Food_Marker.png';
+import Generic from '../Assets/Generic_Marker.png';
 
 export class FreebMarker extends React.Component {
+
+    selectMarker(type) {
+        switch(type) {
+            case '0':
+                return Coffee;
+            case '1':
+                return Candy;
+            case '2':
+                return Food;
+            default:
+                return Generic;
+        }
+    }
+
     render() {
         const { title, latitude, longitude, type } = this.props;
         return (
@@ -11,7 +30,17 @@ export class FreebMarker extends React.Component {
                 latitude: latitude,
                 longitude: longitude,
                 }}
-            />
+            >
+                <Image source={this.selectMarker(type)} style={styles.image} />
+            </Marker>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    image: {
+      aspectRatio: 361/531,
+      width: 75,
+      height: 75,
+    },
+  });
