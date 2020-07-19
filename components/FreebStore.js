cf31326f1e461453120c1195bb080ca866506f8e
Andrew Borgan <andrewborgan88@gmail.com>
2020-07-19 19:47:36 -0400

    add error and loading screens

--- components/FreebStore.js
+++ components/FreebStore.js
@@ -2,6 +2,7 @@ import firestore from '@react-native-firebase/firestore';
 
 export const getStores = async () => {
   const snapshot = await firestore().collection('storesTest').get();
+  if(!Array.isArray(snapshot.docs)) throw new Error("Unable to find Freebies");
   return snapshot.docs.map(doc => docApiMap(doc));
 };
 
